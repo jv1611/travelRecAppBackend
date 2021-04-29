7f9ebc34f8ab78e1cb9834d88dff5d82631c6765
Jochen Vervoort <jochenvervoort@gmail.com>
2021-04-29 12:09:15 +0200

    Created by copy directory

---
+++ tables/post.js
@@ -0,0 +1,19 @@
+var table = module.exports = require('azure-mobile-apps').table();
+
+table.read(function (context) {
+    return context.execute();
+});
+
+table.insert(function (context) {
+    // voorbeeld hieronder straks weghalen
+    context.item.createdAt = DateTime(); // zou moeten werken
+    return context.execute();
+});
+
+table.update(function (context) {
+    return context.execute();
+});
+
+table.delete(function (context) {
+    return context.execute();
+});
